893279e606742382aa407dcb5487038e4bd29571
Subham Das <sd626334@gmail.com.com>
2024-05-01 16:13:48 +0530

    subham

---
+++ main.js
@@ -0,0 +1,295 @@
+var hovercard = document.getElementById("hovercard");
+var gola = document.getElementById("gola");
+
+hovercard.addEventListener(
+  "mouseover",
+  () => (
+    (hovercard.style.backgroundColor = "#B1B493"),
+    (hovercard.style.color = "white")
+  )
+);
+hovercard.addEventListener(
+  "mouseleave",
+  () => (
+    (hovercard.style.backgroundColor = "white"),
+    (hovercard.style.color = "black")
+  )
+);
+hovercard.addEventListener(
+  "mouseover",
+  () => (gola.style.backgroundColor = "white")
+);
+hovercard.addEventListener("mouseover", () =>
+  setTimeout(() => {
+    gola.style.marginLeft = "5rem";
+  }, 50)
+);
+hovercard.addEventListener(
+  "mouseleave",
+  () => (gola.style.backgroundColor = "#B1B493")
+);
+hovercard.addEventListener("mouseleave", () =>
+  setTimeout(() => {
+    gola.style.marginLeft = "2rem";
+  }, 50)
+);
+
+
+var hovercard2 = document.getElementById("hovercard2");
+var gola2 = document.getElementById("gola2");
+
+hovercard2.addEventListener(
+  "mouseover",
+  () => (
+    (hovercard2.style.backgroundColor = "#B1B493"),
+    (hovercard2.style.color = "white")
+  )
+);
+hovercard2.addEventListener(
+  "mouseleave",
+  () => (
+    (hovercard2.style.backgroundColor = "white"),
+    (hovercard2.style.color = "black")
+  )
+);
+hovercard2.addEventListener(
+  "mouseover",
+  () => (gola2.style.backgroundColor = "white")
+);
+hovercard2.addEventListener("mouseover", () =>
+  setTimeout(() => {
+    gola2.style.marginLeft = "5rem";
+  }, 50)
+);
+hovercard2.addEventListener(
+  "mouseleave",
+  () => (gola2.style.backgroundColor = "#B1B493")
+);
+hovercard2.addEventListener("mouseleave", () =>
+  setTimeout(() => {
+    gola2.style.marginLeft = "2rem";
+  }, 50)
+);
+
+
+var hovercard3 = document.getElementById("hovercard3");
+var gola3 = document.getElementById("gola3");
+
+hovercard3.addEventListener(
+  "mouseover",
+  () => (
+    (hovercard3.style.backgroundColor = "#B1B493"),
+    (hovercard3.style.color = "white")
+  )
+);
+hovercard3.addEventListener(
+  "mouseleave",
+  () => (
+    (hovercard3.style.backgroundColor = "white"),
+    (hovercard3.style.color = "black")
+  )
+);
+hovercard3.addEventListener(
+  "mouseover",
+  () => (gola3.style.backgroundColor = "white")
+);
+hovercard3.addEventListener("mouseover", () =>
+  setTimeout(() => {
+    gola3.style.marginLeft = "5rem";
+  }, 50)
+);
+hovercard3.addEventListener(
+  "mouseleave",
+  () => (gola3.style.backgroundColor = "#B1B493")
+);
+hovercard3.addEventListener("mouseleave", () =>
+  setTimeout(() => {
+    gola3.style.marginLeft = "2rem";
+  }, 50)
+);
+
+
+var hovercard4 = document.getElementById("hovercard4");
+var gola4 = document.getElementById("gola4");
+
+hovercard4.addEventListener(
+  "mouseover",
+  () => (
+    (hovercard4.style.backgroundColor = "#B1B493"),
+    (hovercard4.style.color = "white")
+  )
+);
+hovercard4.addEventListener(
+  "mouseleave",
+  () => (
+    (hovercard4.style.backgroundColor = "white"),
+    (hovercard4.style.color = "black")
+  )
+);
+hovercard4.addEventListener(
+  "mouseover",
+  () => (gola4.style.backgroundColor = "white")
+);
+hovercard4.addEventListener("mouseover", () =>
+  setTimeout(() => {
+    gola4.style.marginLeft = "5rem";
+  }, 50)
+);
+hovercard4.addEventListener(
+  "mouseleave",
+  () => (gola4.style.backgroundColor = "#B1B493")
+);
+hovercard4.addEventListener("mouseleave", () =>
+  setTimeout(() => {
+    gola4.style.marginLeft = "2rem";
+  }, 50)
+);
+
+
+var hovercard5 = document.getElementById("hovercard5");
+var gola5 = document.getElementById("gola5");
+
+hovercard5.addEventListener(
+  "mouseover",
+  () => (
+    (hovercard5.style.backgroundColor = "#B1B493"),
+    (hovercard5.style.color = "white")
+  )
+);
+hovercard5.addEventListener(
+  "mouseleave",
+  () => (
+    (hovercard5.style.backgroundColor = "white"),
+    (hovercard5.style.color = "black")
+  )
+);
+hovercard5.addEventListener(
+  "mouseover",
+  () => (gola5.style.backgroundColor = "white")
+);
+hovercard5.addEventListener("mouseover", () =>
+  setTimeout(() => {
+    gola5.style.marginLeft = "5rem";
+  }, 50)
+);
+hovercard5.addEventListener(
+  "mouseleave",
+  () => (gola5.style.backgroundColor = "#B1B493")
+);
+hovercard5.addEventListener("mouseleave", () =>
+  setTimeout(() => {
+    gola5.style.marginLeft = "2rem";
+  }, 50)
+);
+
+
+var hovercard6 = document.getElementById("hovercard6");
+var gola6 = document.getElementById("gola6");
+
+hovercard6.addEventListener(
+  "mouseover",
+  () => (
+    (hovercard6.style.backgroundColor = "#B1B493"),
+    (hovercard6.style.color = "white")
+  )
+);
+hovercard6.addEventListener(
+  "mouseleave",
+  () => (
+    (hovercard6.style.backgroundColor = "white"),
+    (hovercard6.style.color = "black")
+  )
+);
+hovercard6.addEventListener(
+  "mouseover",
+  () => (gola6.style.backgroundColor = "white")
+);
+hovercard6.addEventListener("mouseover", () =>
+  setTimeout(() => {
+    gola6.style.marginLeft = "5rem";
+  }, 50)
+);
+hovercard6.addEventListener(
+  "mouseleave",
+  () => (gola6.style.backgroundColor = "#B1B493")
+);
+hovercard6.addEventListener("mouseleave", () =>
+  setTimeout(() => {
+    gola6.style.marginLeft = "2rem";
+  }, 50)
+);
+
+
+var hovercard7 = document.getElementById("hovercard7");
+var gola7 = document.getElementById("gola7");
+
+hovercard7.addEventListener(
+  "mouseover",
+  () => (
+    (hovercard7.style.backgroundColor = "#B1B493"),
+    (hovercard7.style.color = "white")
+  )
+);
+hovercard7.addEventListener(
+  "mouseleave",
+  () => (
+    (hovercard7.style.backgroundColor = "white"),
+    (hovercard7.style.color = "black")
+  )
+);
+hovercard7.addEventListener(
+  "mouseover",
+  () => (gola7.style.backgroundColor = "white")
+);
+hovercard7.addEventListener("mouseover", () =>
+  setTimeout(() => {
+    gola7.style.marginLeft = "5rem";
+  }, 50)
+);
+hovercard7.addEventListener(
+  "mouseleave",
+  () => (gola7.style.backgroundColor = "#B1B493")
+);
+hovercard7.addEventListener("mouseleave", () =>
+  setTimeout(() => {
+    gola7.style.marginLeft = "2rem";
+  }, 50)
+);
+
+
+
+var hovercard8 = document.getElementById("hovercard8");
+var gola8 = document.getElementById("gola8");
+
+hovercard8.addEventListener(
+  "mouseover",
+  () => (
+    (hovercard8.style.backgroundColor = "#B1B493"),
+    (hovercard8.style.color = "white")
+  )
+);
+hovercard8.addEventListener(
+  "mouseleave",
+  () => (
+    (hovercard8.style.backgroundColor = "white"),
+    (hovercard8.style.color = "black")
+  )
+);
+hovercard8.addEventListener(
+  "mouseover",
+  () => (gola8.style.backgroundColor = "white")
+);
+hovercard8.addEventListener("mouseover", () =>
+  setTimeout(() => {
+    gola8.style.marginLeft = "5rem";
+  }, 50)
+);
+hovercard8.addEventListener(
+  "mouseleave",
+  () => (gola8.style.backgroundColor = "#B1B493")
+);
+hovercard8.addEventListener("mouseleave", () =>
+  setTimeout(() => {
+    gola8.style.marginLeft = "2rem";
+  }, 50)
+);
